e116988eb7a1d29509b19041a6717bae72c2e73d
Sureshraja Singaravadivelu <ssingaravadivelu@adhslx.com>
2019-11-24 19:07:54 +0800

    Websocket_Camel_SBoot | Changed the message sender and receiver operations.

--- src/main/java/com/websocket/websocketcamelspringboot/MessageWSRoute.java
+++ src/main/java/com/websocket/websocketcamelspringboot/MessageWSRoute.java
@@ -11,66 +11,54 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MessageWSRoute extends RouteBuilder {
-	
-	static Logger logger = LoggerFactory.getLogger(MessageWSRoute.class);
-	
-	private final String CONNECTION_URI = "//localhost:9292/chart"; 
 
-	@Override
-	public void configure() throws Exception {
-		
-		onException(Exception.class).log("oops EofException");
-		
-			//{ "header":"heartbeat", "sender":"client", "senderTimestamp":"1212","content":"1212121212"}
-		from("websocket:"+CONNECTION_URI)
-		.routeId("chartRoute")
-        .log(LoggingLevel.INFO,">> msg recieved : ${body}")
-        .unmarshal().json(JsonLibrary.Jackson, WsMessage.class)
-        .process(new Processor() {
-			
-			@Override
-			public void process(Exchange exchange) throws Exception {
-				WsMessage info = exchange.getIn().getBody(WsMessage.class);
-				
-				if( "cmd".equals(info.getHeader()))
-				{
-					if( "start".equals(info.getContent()))
-						exchange.getContext().startRoute("timerRoute");
-					if( "stop".equals(info.getContent()))
-						exchange.getContext().stopRoute("timerRoute");
-				}				
-				
-				logger.info("data from client: " + info);
-				WsMessage response = new WsMessage();
-				response.setHeader("ping");
-				response.setSender("server");
-				response.setSenderTimestamp("" + System.currentTimeMillis());
-				response.setContent("pong");
-				response.setContentType("Response CONTENT");
-				
-				exchange.getIn().setBody(response, WsMessage.class);				
-			}
-		})
-		//.marshal().json(JsonLibrary.Jackson, WsMessage.class)
-		.to("websocket:"+CONNECTION_URI).log(LoggingLevel.INFO,">> msg sent back to client : ${body}");	// intern camel kø
+    static Logger logger = LoggerFactory.getLogger(MessageWSRoute.class);
 
+    private final String CONNECTION_URI = "//localhost:9090/chart";
 
-		from("websocket:"+CONNECTION_URI+"/1")
-				.routeId("chartRoute1")
-				.log(LoggingLevel.INFO,">> msg recieved : ${body}")
-				.process(new Processor() {
+    @Override
+    public void configure() throws Exception {
 
-					@Override
-					public void process(Exchange exchange) throws Exception {
-						String info = exchange.getIn().getBody(String.class);
+        onException(Exception.class).log("oops EofException");
+
+        //{ "header":"heartbeat", "sender":"client", "senderTimestamp":"1212","content":"1212121212"}
+        from("websocket:" + CONNECTION_URI)
+                .routeId("chartRoute")
+                .log(LoggingLevel.INFO, ">> msg recieved : ${body}")
+                .unmarshal().json(JsonLibrary.Jackson, WsMessage.class)
+                .process(new Processor() {
+
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        WsMessage info = exchange.getIn().getBody(WsMessage.class);
+
+                        logger.info("data from client: " + info);
+                        WsMessage response = new WsMessage();
+                        response.setHeader("ping");
+                        response.setSender("server");
+                        response.setSenderTimestamp("" + System.currentTimeMillis());
+                        response.setContent("pong");
+                        response.setContentType("Response CONTENT to "+info.getSender());
+
+                        exchange.getIn().setBody(response, WsMessage.class);
+                    }
+                })
+                //.marshal().json(JsonLibrary.Jackson, WsMessage.class)
+                .to("websocket:" + CONNECTION_URI).log(LoggingLevel.INFO, ">> msg sent back to client : ${body}");    // intern camel kø
+
+        from("websocket:" + CONNECTION_URI + "/1")
+                .routeId("chartRoute1")
+                .log(LoggingLevel.INFO, ">> msg recieved : ${body}")
+                .process(new Processor() {
+
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        String info = exchange.getIn().getBody(String.class);
 
 
-						exchange.getIn().setBody(info + " Response");
-					}
-				})
-				.to("websocket:"+CONNECTION_URI+"/1").log(LoggingLevel.INFO,">> msg sent back to client : ${body}");	// intern camel kø
-
-		
-
-	}
+                        exchange.getIn().setBody(info + " Response");
+                    }
+                })
+                .to("websocket:" + CONNECTION_URI + "/1").log(LoggingLevel.INFO, ">> msg sent back to client : ${body}");    // intern camel kø
+    }
 }
